59df4b4416d744489f170b2ade74a66bca27a85d
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-02-01 16:24:21 +0200

    correct Formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Formik, ErrorMessage } from 'formik';
 import * as yup from 'yup';
@@ -10,61 +10,59 @@ import {
   ErrorText,
 } from './ContactForm.styled';
 
-class ContactForm extends Component {
-  handleSubmit = (values, { resetForm }) => {
-    this.props.onSubmit(values);
+function ContactForm({ onSubmit }) {
+  const handleSubmit = (values, { resetForm }) => {
+    onSubmit(values);
     resetForm();
   };
 
-  schema = yup.object().shape({
+  const schema = yup.object().shape({
     name: yup.string().required(),
     number: yup.string().required(),
   });
 
-  render() {
-    return (
-      <>
-        <Formik
-          initialValues={{ name: '', number: '' }}
-          validationSchema={this.schema}
-          onSubmit={this.handleSubmit}
-        >
-          <FormBoxStyled autoComplete="off">
-            <LabelStyled>
-              Name
-              <InputStyled
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-              />
-            </LabelStyled>
-            <ErrorMessage
+  return (
+    <>
+      <Formik
+        initialValues={{ name: '', number: '' }}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        <FormBoxStyled autoComplete="off">
+          <LabelStyled>
+            Name
+            <InputStyled
+              type="text"
               name="name"
-              render={message => <ErrorText>{message}</ErrorText>}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
             />
+          </LabelStyled>
+          <ErrorMessage
+            name="name"
+            render={message => <ErrorText>{message}</ErrorText>}
+          />
 
-            <LabelStyled>
-              Number
-              <InputStyled
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-              />
-            </LabelStyled>
-            <ErrorMessage
+          <LabelStyled>
+            Number
+            <InputStyled
+              type="tel"
               name="number"
-              render={message => <ErrorText>{message}</ErrorText>}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
             />
-            <ButtonStyled type="submit">Add contact</ButtonStyled>
-          </FormBoxStyled>
-        </Formik>
-      </>
-    );
-  }
+          </LabelStyled>
+          <ErrorMessage
+            name="number"
+            render={message => <ErrorText>{message}</ErrorText>}
+          />
+          <ButtonStyled type="submit">Add contact</ButtonStyled>
+        </FormBoxStyled>
+      </Formik>
+    </>
+  );
 }
 
 export default ContactForm;
